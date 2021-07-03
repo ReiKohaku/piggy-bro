e4a974711dee74d144b760ed5724d8f0462bc00c
ReiKohaku <hbsjzjwj@163.com>
2021-07-03 22:44:21 +0800

    feat: Add "Template"

--- src/bot.ts
+++ src/bot.ts
@@ -1,13 +1,17 @@
 import {Wechaty, Message, Contact} from "wechaty"
 
 import path from "path"
-import SqliteTemplate from "./lib/SqliteTemplate";
 import {mkdirSync} from "./lib/Util";
 export const __data_dir = path.join(__dirname, "../data")
-
 mkdirSync(__data_dir)
+
+import SqliteTemplate from "./lib/SqliteTemplate";
 const sqliteTemplate = new SqliteTemplate(path.join(__data_dir, "./database.db"))
 export {sqliteTemplate}
+
+import Template from "./lib/Template";
+const template = new Template()
+export {template}
 
 const wechaty = Wechaty.instance()
 wechaty.on("scan", (qrcode, status) => {
@@ -19,7 +23,6 @@ wechaty.on("login", (user: Contact) => {
     console.log(`User ${user.name()} logged in.`)
 })
 wechaty.on("message", async (message: Message) => {
-    console.log(`Message: ${message.text()}`)
     const {mp} = await import("./interceptor")
     const cmdResult = await mp.process(message)
     if (cmdResult) await message.say(cmdResult)
---
+++ src/lib/Template.ts
@@ -0,0 +1,46 @@
+/*
+  Template 类的作用是提供一个文本模板。
+  预先设定好模板后，在指定的地方使用模板并提供变量，
+  就可以返回预先设定好的应答。
+  这样设计的优点是方便随时替换应答文本，简化代码。
+ */
+export default class Template {
+    private templates: Record<string, string[] | string>
+    constructor() {
+        this.templates = {}
+    }
+
+    /**
+     * 添加指定键值。
+     * 如果目标键已经有了值，将不会做任何改动。
+     */
+    public add(name: string, value: string | string[]): void {
+        if (!this.templates[name]) this.templates[name] = value
+    }
+
+    /**
+     * 设定指定键值。如果先前键值包含一个值，将返回这个值。
+     * 这个方法和 add() 的区别是，如果指定键已经有了值将不会替换，但这个方法会。
+     */
+    public set(name: string, value: string | string[]): string | string[] | void {
+        const ori = this.templates[name]
+        this.templates[name] = value
+        if (ori) return ori
+    }
+
+    public use(name: string, args: Record<string, any> | Array<any>): string {
+        if (!this.templates[name]) {
+            console.warn("Cannot find key \"" + name + "\", will return original key.")
+            return name
+        }
+        const originalValue = this.templates[name]
+        const value: string = Array.isArray(originalValue) ?
+            originalValue[Math.floor(Math.random() * originalValue.length)] :
+            originalValue
+        return value.replace(/{.*?}/g, function (match) {
+            const matchedArg = match.match(/{(.*)}/)[1]
+            if (args[matchedArg]) return args[matchedArg]
+            else return match
+        })
+    }
+}
