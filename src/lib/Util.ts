13e3e26da095fad22d663a5c0a40d29075bb02af
ReiKohaku <hbsjzjwj@163.com>
2021-08-04 16:24:34 +0800

    feat: Add getMethodConfig util

--- src/lib/Util.ts
+++ src/lib/Util.ts
@@ -1,5 +1,6 @@
 import path from "path";
 import fs from "fs";
+import {__data_dir} from "../bot";
 
 export function mkdirSync(dirPath: string) {
     if (dirPath === null || dirPath === "") return
@@ -24,4 +25,17 @@ function isAbsolute(filePath: string) {
     if (filePath.substring(0, 1) === "/") return true
     return filePath.search(/[\w]+:/) === 0
 
+}
+
+export function getMethodConfig<T extends {} = {}>(name: string) {
+    try {
+        const configPath = path.join(__data_dir, `config/method/${name}.json`)
+        if (fs.existsSync(configPath)) {
+            const fileData = fs.readFileSync(configPath, "utf-8")
+            return JSON.parse(fileData) as T
+        }
+    } catch {
+
+    }
+    return {} as T
 }
